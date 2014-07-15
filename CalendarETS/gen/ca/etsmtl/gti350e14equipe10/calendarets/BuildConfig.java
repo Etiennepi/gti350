d00140884cf4600faf5779fb65a7649b813dbc91
jbsimard <jean-benoit.simard.1@ens.etsmtl.ca>
2014-07-15 15:41:53 -0400

    Structure du projet android par défaut

---
+++ CalendarETS/gen/ca/etsmtl/gti350e14equipe10/calendarets/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ca.etsmtl.gti350e14equipe10.calendarets;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
